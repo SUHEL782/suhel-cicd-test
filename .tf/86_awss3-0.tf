e5f851db2f63ddd1c08932592339f0ab1298ea4f
suhel <thecomputersk@gamil.com>
2025-11-02 02:38:14 +0530

    Completed Madatcloud CICD Terraform test

--- .tf/86_awss3-0.tf
+++ .tf/86_awss3-0.tf
@@ -1,6 +1,6 @@
 # Terraform config for Amazon S3
 resource "awss3" "awss3-0" {
-  name = "example-awss3-0"
+  name = "Suhel-awss3-0"
 }
 
 # Operation: events (Source (S3) sends events to target (Lambda). Need S3 bucket notification and lambda permission. Lambda needs basic execution role.)
